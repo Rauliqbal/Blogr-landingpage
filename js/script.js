105c271f5e2ffdf1b63c63880f1e22639286221b
Rauliqbal <muhamadrauliqbal.13@gmail.com>
2022-02-01 07:23:52 +0700

    add navbar and submenu

--- js/script.js
+++ js/script.js
@@ -1,15 +1,9 @@
 const navbar = document.querySelector(".nav .nav-menu");
 const menuBtn = document.querySelector(".fa-bars");
-const btnSubMenu = document.querySelector(".btn-submenu");
-const subMenu = document.querySelector(".sub-menu");
 
 menuBtn.onclick = () => {
     menuBtn.classList.toggle("fa-times");
     navbar.classList.toggle("active");
-};
-
-btnSubMenu.onclick = () => {
-    subMenu.classList.toggle("active");
 };
 
 window.onscroll = () => {
